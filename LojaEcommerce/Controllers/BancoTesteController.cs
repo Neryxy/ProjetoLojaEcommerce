dd859e0a54975dd5b5d4543dc06d047c360eb69d
Vitor <vitormortagonery@gmail.com>
2026-04-23 02:27:49 -0300

    feat: Criando a tela de Login e validação no MySQL

---
+++ LojaEcommerce/Controllers/BancoTesteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+namespace LojaEcommerce.Controllers
+{
+    public class BancoTesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            string conexao = "server=localhost;database=dbProjetoLoja;uid=root;pwd=vitor230908@;";
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(conexao)) 
+                {
+                    conn.Open();
+                    ViewBag.Mensagem = "Conectado com Sucesso ao Banco!";
+                }
+            }
+            catch (Exception ex)
+            { 
+                ViewBag.Mensagem = "Erro na Conexão" + ex.Message;
+            }
+            return View();
+        }
+    }
+}
+
---
+++ LojaEcommerce/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaEcommerce.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
